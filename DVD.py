d422cbfbc42d47a4966ec579e883cf92e61715bd
Alexander M <axlmalyasov@gmail.com>
2022-02-11 19:02:48 +1000

    DVD project - bext module installed

--- DVD.py
+++ DVD.py
@@ -1,2 +1,14 @@
-'''Анимация отскакивающего от краёв логотипа DVD'''
+'''Анимация отскакивающего от краёв логотипа DVD
+CTRL+C для выхода
+Не меняйте размер окна во время работы программы'''
 
+import sys, random, time
+
+try:
+    import bext
+except ImportError:
+    print('Эта программа требует установленного модуля bext. \n Можно установить командой pip3 install bext')
+
+# задаем константы
+WIDTH, HEIGHT = bext.size()
+print(WIDTH, HEIGHT)
