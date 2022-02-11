6c2863108c5cfea9e5408f71678e2b6f426abd9d
Alexander M <axlmalyasov@gmail.com>
2022-02-11 20:11:10 +1000

    DVD project - change direction

--- DVD.py
+++ DVD.py
@@ -30,8 +30,77 @@ X = 'x'
 Y = 'y'
 DIR = 'direction'
 
+
 def main():
-    pass
+    bext.clear()
+
+    # генерация логотипов
+    logos = []
+    for i in range(NUMBER_OF_LOGOS):
+        logos.append({COLOR: random.choice(COLORS),
+                    X: random.randint(1, WIDTH - 4),
+                    Y: random.randint(1, HEIGHT - 4),
+                    DIR: random.choice(DIRECTION)})
+        if logos[-1][X] % 2 == 1:
+            # гарантируем что Х четное число, для столкновения с углом
+            logos[-1][X] -= 1
+
+    cornerBounces = 0  # посчитать сколько раз логотип столкнулся с углом
+    while True:  # основной цикл программы
+        for logo in logos:  # обработка всех логотипов в списке
+            bext.goto(logo[X], logo[Y])
+            print('   ', end='')
+
+            originalDirection = logo[DIR]
+
+            # проверка, не отскакивает ли логотип от угла
+            if logo[X] == 0 and logo[Y] == 0:
+                logo[DIR] = DOWN_RIGHT
+                cornerBounces += 1
+            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
+                logo[DIR] == UP_RIGHT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
+                logo[DIR] = DOWN_LEFT
+                cornerBounces += 1
+            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
+                logo[DIR] = UP_LEFT
+                cornerBounces += 1
+
+            # если логотип отскакивает от левого края
+            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] == UP_RIGHT
+            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] == DOWN_RIGHT
+
+            # если отскакивает от правого края
+            # WIDTH - 3 так как DVD состоит из 3 букв
+            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
+                logo[DIR] == UP_LEFT
+            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] == DOWN_LEFT
+
+            # если логотип отскакивает от верхнего края
+            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
+                logo[DIR] == DOWN_LEFT
+            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
+                logo[DIR] == DOWN_RIGHT
+
+            # если лого отскакивает от нижнего края
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
+                logo[DIR] == UP_LEFT
+            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
+                logo[DIR] == UP_RIGHT
+
+            if logo[DIR] != originalDirection:
+                # поменять цвет при отскакивании логотипа
+                logo[COLOR] = random.choice(COLORS)
+
+
+
+
+    # print(logos)
+
 
 
 if __name__ == '__main__':
