203862955b9b1a8c222fe688ad96934ddf7a4305
Alexander M <axlmalyasov@gmail.com>
2022-02-11 19:34:50 +1000

    DVD project - constants

--- DVD.py
+++ DVD.py
@@ -11,4 +11,33 @@ except ImportError:
 
 # задаем константы
 WIDTH, HEIGHT = bext.size()
-print(WIDTH, HEIGHT)
+# print(WIDTH, HEIGHT)
+# в Windows нельзя вывести символ в последний столбец без дообавления
+# автоматически символа новой строки, поэтому уменьшаем нирину на 1
+WIDTH -= 1
+NUMBER_OF_LOGOS = 5
+PAUSE_AMOUNT = 0.2
+COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+UP_RIGHT = 'ur'
+UP_LEFT = 'ul'
+DOWN_RIGHT = 'dr'
+DOWN_LEFT = 'dl'
+DIRECTION = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+
+# ключи для ассоциативных массивов логотипов
+COLOR = 'color'
+X = 'x'
+Y = 'y'
+DIR = 'direction'
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:   # при нажатии CTRL + C завершить выполнение
+        print()
+        print('Отскакивающий DVD-логотип')
+        sys.exit()
