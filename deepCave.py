09653744730d98406ee858c3e03729e1deedcaf3
Alexander M <axlmalyasov@gmail.com>
2022-05-11 23:34:35 +1000

    DeepCave project completed and tested Diamond project started

--- deepCave.py
+++ deepCave.py
@@ -16,3 +16,31 @@ time.sleep(3)
 leftWidth = 20  # колличество "#" слева от пещеры
 gapWidth = 10   # коллличество пробелов (ширина пещеры)
 
+while True:
+    # отобразить фрагмент туннеля
+    rightWidth = WIDTH - leftWidth - gapWidth   # чтобы ширина строки была постоянной
+    print(('#' * leftWidth) + (' ' * gapWidth) + ('#' * rightWidth))
+
+    # проверяем не нажато ли Ctrl + C во время короткой паузы
+    try:
+        time.sleep(PAUSE_AMOUNT)
+    except KeyboardInterrupt:
+        sys.exit()
+
+    # подборка ширины левой части
+    diceRoll = random.randint(1, 6)
+    if diceRoll == 1 and leftWidth > 1:
+        leftWidth = leftWidth - 1
+    if diceRoll == 2 and leftWidth + gapWidth < WIDTH - 1:
+        leftWidth = leftWidth + 1
+    else:
+        pass
+
+    # подбираем ширину пещеры (если нам нужен этот код)
+    diceRoll = random.randint(1, 6)
+    if diceRoll == 1 and gapWidth > 1:
+       gapWidth = gapWidth - 1
+    elif diceRoll == 2 and leftWidth + gapWidth < WIDTH - 1:
+       gapWidth = gapWidth + 1
+    else:
+       pass  # Ничего не делаем; ширина зазора не меняется.
